4477b5c04467fe45a66a487fab491699db52735b
Felix Bayer <felix.bayer16@web.de>
2025-03-20 20:45:07 +0100

    Initial commit

---
+++ autostart.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# background image
+BACKGROUND=$(cat ~/wallpapers/current.txt)
+
+# Setze den Hintergrund für den Sway Desktop
+#swaymsg output "*" bg "$BACKGROUND" fill
+
+# sound
+
+#volumeicon &
+#pulseaudio &
+#
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#nm-applet &
+#xfce4-power-manager &
+#pamac-tray &
+#clipit &
+#
+#redshift-gtk -l 50:7.7 &
+#
+#sxhkd -c ~/.config/i3/sxhkdrc
+
